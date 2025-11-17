30ea24ab63e16cc7fa9529296dff7cb17b1e78b7
Beni <20230059@uthh.edu.mx>
2025-11-17 11:31:15 -0600

    Fix para vercel

--- api/index.js
+++ api/index.js
@@ -1,5 +1,21 @@
-const app = require("../index");
+const express = require("express");
+const cors = require("cors");
+const conectarDB = require("../config/database");
+const usuarioRoutes = require("../routes/usuarioRoutes");
+require("dotenv").config();
 
-module.exports = (req, res) => {
-  app(req, res);
-};
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+conectarDB();
+
+app.get("/", (req, res) => {
+  res.json({ mensaje: "API funcionando correctamente desde /api" });
+});
+
+app.use("/benilde", usuarioRoutes);
+
+// Exportar como serverless function
+module.exports = app;
